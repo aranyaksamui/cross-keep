85acaed799f3b84a2df76eca10dc43466848619e
Aranyak Samui <aranyakarya@gmail.com>
2022-07-07 09:21:16 +0530

    Add style.css and public folder for managing static files, add header and footer boilerplates, add .env file and modify some js files.

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ const app = express();
 // Middlewares
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 // Handling routes and requests
 app.route("/")
     .get((req, res) => {
-        res.render("index", { heading: heading })
+        res.render("index")
     })
     .post();
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
-})
+});
